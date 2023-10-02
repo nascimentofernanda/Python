f1f83c31e9b9dfaf79aaaeb8a1f04796eb34d5a2
fercfn <nascimentofernandadev@gmail.com>
2023-10-01 21:34:15 -0300

    resolução de exercícios

---
+++ Exercícios/Desafio_100_exercícios/ex_007.py
@@ -0,0 +1,5 @@
+#Faça um programa que leia as duas notas de um aluno, calcule e mostre sua média
+nota_um = float(input('Digite a primeira nota \n'))
+nota_dois = float(input('Digite a segunda nota \n'))
+média = (nota_um + nota_dois)/2
+print(f'A média das notas é {média}')
