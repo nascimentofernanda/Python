6d703e76fb68c646bc656ec5aa8c6d68e206bd25
fercfn <nascimentofernandadev@gmail.com>
2023-10-01 21:16:59 -0300

    resolução de exercícios

--- Exercícios/Desafio_100_exercícios/ex_002.py
+++ Exercícios/Desafio_100_exercícios/ex_002.py
@@ -1,3 +1,3 @@
 #Crie um programa que peça o nome do usuário e imprima a mensagem "É um prazer te conhecer, nome do usuário!"
 nome = input('Digite seu nome \n')
-print('É um prazer te conhecer, {}!'.format(nome))
+print(f'É um prazer te conhecer, {nome}!')
---
+++ Exercícios/Desafio_100_exercícios/ex_003.py
@@ -0,0 +1,5 @@
+#Crie um programa que leia dois números e mostre a soma entre eles
+numero_um = int(input('Digite um número \n'))
+numero_dois = int(input('Digite um número \n'))
+soma = numero_um + numero_dois
+print(f'A soma dos números é {soma}')
