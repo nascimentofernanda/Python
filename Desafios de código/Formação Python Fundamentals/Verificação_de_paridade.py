22dbb23c1bbebdf0b0a2553028254f6877400112
fercfn <nascimentofernandadev@gmail.com>
2024-09-05 12:13:08 -0300

    Alteração de arquivos e resolução de exercício

---
+++ Desafios de código/Formação Python Fundamentals/Verificação_de_paridade.py
@@ -0,0 +1,8 @@
+#Neste desafio, você deve escreva uma solução que receba um número inteiro como entrada e determine se ele é par ou ímpar. 
+# Dessa forma, a solução deve retornar uma string indicando Par se o número for par e Ímpar se o número for ímpar.
+
+num=int(input())
+if num%2==0:
+    print("Par")
+else:
+    print("Ímpar")
